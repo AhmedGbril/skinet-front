adf4cf72404b6ed7638f72c220b246d4f8d74938
Ahmed Abdelhamied <AhmedAAbdelhamied@outlook.com>
2023-10-29 22:12:44 +0300

    Angular Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'Skinet';
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5001/api/product').subscribe(
+      {
+        next:(response:any)=>console.log(response),
+        error:rr=> console.log(rr),
+        complete:()=>{
+          console.log('requset Completed');
+          console.log('extra statment'); 
+        }
+      }
+    )
+  }
+
 }
