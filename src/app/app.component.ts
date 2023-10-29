6e5bbc497a0670446204aaf555a3ad346647a58b
AmrAyman-G <100245739+AmrAyman-G@users.noreply.github.com>
2023-10-29 23:40:06 +0200

    showing product in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent  implements OnInit{
   title = 'Skinet';
+  Products :any[]=[];
 
   constructor(private http:HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5001/api/product').subscribe(
+    this.http.get('http://localhost:5001/api/product?pagesize=50').subscribe(
       {
-        next:(response:any)=>console.log(response),
+        next:(response:any)=>this.Products=response.data,
         error:rr=> console.log(rr),
         complete:()=>{
           console.log('requset Completed');
